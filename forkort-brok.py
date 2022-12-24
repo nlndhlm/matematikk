2511939a5e4359de59d2b5dd8b74e1183b746f3e
nlndhlm <30705699+nlndhlm@users.noreply.github.com>
2022-12-24 11:48:30 +0100

    Update forkort-brok.py

--- forkort-brok.py
+++ forkort-brok.py
@@ -1,21 +1,14 @@
 def primtallsfaktoriser(tall):
-
     i = 2
-
     faktorer = []
-
     while i <= tall:
         if tall % i == 0:
             faktorer.append(i)
-
             tall = tall / i
             i = 2
-
         else:
             i = i + 1
-
     faktorer.sort(reverse=True) # sorterer i stigende rekkerfølge
-
     return faktorer
 
 
